1439fed664c9c8bde799f62b745e341ee6fed9a5
shohruhbek-dev <shohruhbek.abdusattarov.205@gmail.com>
2025-03-28 00:06:53 +0500

    test registration

--- src/root.jsx
+++ src/root.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/home";
+import RegistrationForm from "./pages/auth";
 
 function Root() {
- 
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes> 
+        <Route path="/registration" element={<RegistrationForm />} />
+      </Routes>
     </Layout>
   );
 }
