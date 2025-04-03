45848dae9300c17379a6dc7992ea9d5b59bb48e8
“Vitali <muhammadaalin01@gmail.com>
2025-04-03 10:49:20 +0500

    images

--- src/components/cards/news/index.jsx
+++ src/components/cards/news/index.jsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import news1 from "/src/assets/news1.avif";
 import news2 from "/src/assets/news2.avif";
@@ -8,31 +11,42 @@ import kid from "/src/assets/kid.avif";
 
 function News() {
     const imgs = [news1, news2, news3, news4, kid];
-    const sliderRef = useRef(null);
-    const [clones, setClones] = useState([]);
 
-    useEffect(() => {
-        setClones([...imgs, ...imgs]);
-    }, []);
-
-    useEffect(() => {
-        const slider = sliderRef.current;
-        let scrollInterval;
-        
-        const startScrolling = () => {
-            scrollInterval = setInterval(() => {
-                if (slider) {
-                    slider.scrollLeft += 1;
-                    if (slider.scrollLeft >= slider.scrollWidth / 2) {
-                        slider.scrollLeft = 0;
-                    }
-                }
-            }, 15);
-        };
-
-        startScrolling();
-        return () => clearInterval(scrollInterval);
-    }, [clones]);
+    const settings = {
+        infinite: true,
+        speed: 5000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: "linear",
+        arrows: false,
+        pauseOnHover: false,
+        draggable: true,
+        swipe: true,
+        touchMove: true,
+        rtl: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+                },
+            }
+        ],
+    };
 
     return (
         <div className="news px-6 bg-gray-50 flex flex-col items-center overflow-hidden">
@@ -40,14 +54,23 @@ function News() {
                 Qaynoq yangiliklar:
             </h1>
 
-            <div className="w-full overflow-hidden relative">
-                <div ref={sliderRef} className="flex gap-6 w-full overflow-x-scroll no-scrollbar whitespace-nowrap scrollbar-hide" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-                    {clones.map((item, index) => (
-                        <a href="https://www.instagram.com/founders_school?igsh=ZzZ1N3dkMWlkYWJ4" target="_blank" key={index} className="min-w-[250px] 2xl:min-w-[300px] h-auto rounded-xl hover:shadow-2xl hover:scale-[1.02] transition-all duration-300 p-4 flex items-center justify-center">
-                            <img src={item} alt={`News ${index + 1}`} className="w-[380px] h-[300px] rounded-[40px] lazyload" />
+            <div className="w-full relative">
+                <Slider {...settings} className="w-full">
+                    {imgs.map((item, index) => (
+                        <a
+                            href="https://www.instagram.com/founders_school?igsh=ZzZ1N3dkMWlkYWJ4"
+                            target="_blank"
+                            key={index}
+                            className="px-4 flex items-center justify-center"
+                        >
+                            <img
+                                src={item}
+                                alt={`News ${index + 1}`}
+                                className="w-[260px] h-[350px] rounded-[40px] max-[425px]:w-[130px] max-[425px]:h-[170px] max-[425px]:rounded-[13px] hover:shadow-2xl hover:scale-[1.02] transition-all duration-300"
+                            />
                         </a>
                     ))}
-                </div>
+                </Slider>
             </div>
         </div>
     );
